18d7bfbadc550d62ddf83521c501e030315b4fa9
Nithin Daniel <nithindaniel2018@gmail.com>
2024-02-21 17:56:44 +0530

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+'''
+post_id 
+post_title
+post_description
+post_posted_date
+post_last_updated_date
+post_uploaded_time
+post_image_link
+post_like
+post_dislike
+
+
+'''

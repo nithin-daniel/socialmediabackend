d081f46e63146854e87feb165b9bf5364e59e7fb
Nithin Daniel <nithindaniel2018@gmail.com>
2024-02-29 19:21:42 +0530

    working on manytomany

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .serializers import AllPostSerializers,LikeSerializers
 from .models import Post,Like
+from django.contrib.auth import authenticate, login
 # Create your views here.
 class ListPosts(APIView):
     def get(self,request,format=None):
@@ -18,9 +19,31 @@ class PostLikeView(APIView):
         like_id = request.data['like_id']
         like_model = Like.objects.filter(post=like_id)
         like_serializer = LikeSerializers(like_model,many=True)
-        if request.user in like_serializer.data[0]['users']:
-            like_model.users.remove(request.user)
-            return Response("user removed")        
-        else:
-            like_model.users.add(request.user)
-            return Response("user added")
+
+        # hello = Like.objects.all()
+        # print(hello.Users.user)
+        print(like_model.users.add(request.user))
+
+
+        '''For a test purpose done this authentication method'''
+
+        # user = authenticate(request, username="sampleuser", password="sample@123")
+        # if user is not None:
+        #     login(request, user)
+        #     print("done")
+        # else:
+        #     print("nothing done")
+        # if request.user.is_authenticated:
+        #     print("authenticated")
+        # else:
+        #     print("not auth")
+
+
+
+        # if request.user in like_serializer.data[0]['users']:
+        #     like_model.users.remove(request.user)
+        #     return Response("user removed")        
+        # else:
+        #     like_model.users.add(request.user)
+            # return Response("user added")
+        return Response("user added")

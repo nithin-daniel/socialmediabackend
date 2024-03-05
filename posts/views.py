0fd24f4ae14fe06833d8d20730d61efc18b565da
Nithin Daniel <nithindaniel2018@gmail.com>
2024-03-05 18:08:57 +0530

    some commented portion removed

--- posts/views.py
+++ posts/views.py
@@ -27,34 +27,6 @@ class PostLikeView(APIView):
     @swagger_auto_schema(request_body=LikeSerializers)
     
     def post(self, request, format=None):
-        # like_id = request.data['like_id']
-        # like_filter = Like.objects.filter(post__post_id=like_id)
-        # like_user_filter = like_filter.get(users=request.user)
-        # if like_user_filter:
-        #     if like_user_filter.users.exists():
-        #         like_user_filter.users.remove(request.user)
-        #         return Response("User removed", status=status.HTTP_201_CREATED)
-        #     else:
-        #         like_user_filter.users.add(request.user)
-        #         return Response("User added", status=status.HTTP_201_CREATED)
-        # else:
-        #     print("not found")
-
-
-        # try:
-        #     like_id = request.data['like_id']
-        #     like_filter = Like.objects.filter(post__post_id=like_id)
-        #     like_user_filter = like_filter.get(users=request.user)
-
-        #     if like_user_filter.users:
-        #         like_user_filter.users.remove(request.user)
-        #         return Response("User removed", status=status.HTTP_201_CREATED)
-        #     else:
-        #         like_user_filter.users.add(request.user)
-        # except Like.DoesNotExist:
-        #     return Response("User not liked", status=status.HTTP_201_CREATED)   
-
-
         like_id = request.data['like_id']
         like_filter = Like.objects.filter(post__post_id=like_id).first()
         if request.user in like_filter.users.filter(requirement_comment_likes=request.user.id):
@@ -76,31 +48,4 @@ class PostLikeView(APIView):
             print("authenticated")
         else:
             print("not auth")
-    #     print(request.user)
-    #     like_id = request.data['like_id']
-    #     like_model = Like.objects.filter(post=like_id)
-    #     like_serializer = LikeSerializers(like_model, many=True)
-        #   print(request.auth)
-    #     # hello = Like.objects.all()
-    #     # print(hello.Users.user)
-    #     print(like_model.users.add(request.user))
-
-    #     """For a test purpose done this authentication method"""
-
-        # user = authenticate(request, username="sampleuser", password="sample@123")
-        # if user is not None:
-        #     login(request, user)
-        #     print("done")
-        # else:
-        #     print("nothing done")
-        # if request.user.is_authenticated:
-        #     print("authenticated")
-        # else:
-        #     print("not auth")
-
-    #     # if request.user in like_serializer.data[0]['users']:
-    #     #     like_model.users.remove(request.user)
-    #     #     return Response("user removed")
-    #     # else:
-    #     #     like_model.users.add(request.user)
-        return Response("user added")
+        return Response("user auth")
